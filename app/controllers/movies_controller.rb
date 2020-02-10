685d59fdaea823693caad8ad6a43c4ef61df6a9e
EC2 Default User <ec2-user@ip-172-31-22-140.us-east-2.compute.internal>
2020-02-10 03:55:28 +0000

    <first bug b>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -12,17 +12,8 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = ['G','PG','PG-13','R']
-    #@movies = Movie.all
-    if params[:sort].nil? && params[:ratings].nil?
-      return @movies = Movie.all
-    elsif params[:ratings].nil?
-      #@movies = Movie.select do |a,b|
-      #  allowed[:sort] == 'Aladdin'
-      #end
-      return @movies = Moive.all 
-    else
-      #@movies = Moive { |a,b| a['title'] <=> b['title']}
-      return @movies = Moive.all
+    @movies = Movie.all
+    
     end
   end
 
