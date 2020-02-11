25bccc0f2f248be0cae03aafabf4dbfbce915571
EC2 Default User <ec2-user@ip-172-31-22-140.us-east-2.compute.internal>
2020-02-11 00:40:46 +0000

    <first bug b>

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,6 +13,7 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = ['G','PG','PG-13','R']
     @movies = Movie.all.sort_by { |obj| obj[:title] }
+    @sortit = params[:params]
     #@movies.sort_by { |obj| obj[:title] }
     #@movies = Moive.all.order(:release_date)
     return @movies
