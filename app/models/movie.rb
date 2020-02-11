5847f98a89021c14d92d1721982c60cc1b4abaca
EC2 Default User <ec2-user@ip-172-31-22-140.us-east-2.compute.internal>
2020-02-11 04:12:48 +0000

    part 2 start

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,7 +3,8 @@ class Movie < ActiveRecord::Base
         ['G','PG','PG-13','R']
     end
     def self.movie_filter(filters, sorts)
-        return self.order(sorts) if not filters
-        self.where(:rating => filters.keys).order(sort_t)
+        return self.order(sorts)
+        #if not filters
+        #self.where(:rating => filters.keys).order(sort_t)
     end
 end
