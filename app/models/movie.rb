3fcda5f0a918e54cf6dc40b98b640f0cb361c812
EC2 Default User <ec2-user@ip-172-31-22-140.us-east-2.compute.internal>
2020-02-28 03:07:01 +0000

    part 2 start

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,9 +2,9 @@ class Movie < ActiveRecord::Base
     def self.ratings 
         ['G','PG','PG-13','R']
     end
-    scope :ratedscope, lambda { |allow| 
-        Movie.where(rating: allow)
-    }
+    #scope :ratedscope, lambda { |allow| 
+    #    Movie.where(rating: allow)
+    #}
     #def self.movie_filter(filters, sorts)
     #    return self.all.sort_by { |obj| obj[sorts] }
         #if not filters
