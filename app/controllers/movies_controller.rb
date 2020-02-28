33733fa8d465fb3d206c65e8882a4e60fe3b6e98
EC2 Default User <ec2-user@ip-172-31-22-140.us-east-2.compute.internal>
2020-02-28 01:34:27 +0000

    part 2 start

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -16,12 +16,13 @@ class MoviesController < ApplicationController
     @order_by = :title
     @all_ratings = ['G','PG','PG-13','R']
     @test_ratings = ['G','PG','PG-13','R']
+    session[:ratings] = ['G','PG','PG-13','R']
     @ratingread = params[:ratings].nil? ? @test_ratings : params[:ratings].keys
     logger.debug(@ratingread)
     #@ratings = params[:ratings].nil? ? @all_ratings : params[:ratings]
     puts "ratings {#ratings} "
     @sortit = params[:sortit].nil? ? "rating" : params[:sortit]
-    #@movies = Movie.order(@sortit)
+    @movies = Movie.order(@sortit)
     @movies = Movie.where( rating: @ratingread )
     return @movies
     
